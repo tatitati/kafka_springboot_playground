eff5182efa83b2e070fa6449064fd10e4892e187
Francisco Albusac <francisco.albusac@bbc.co.uk>
2020-05-22 12:33:13 +0100

    Fri 22 May 2020 12:33:13 BST

--- src/main/kotlin/com/example/demo/config/AutoCreateConfig.kt
+++ src/main/kotlin/com/example/demo/config/AutoCreateConfig.kt
@@ -1,4 +1,19 @@
 package com.example.demo.config
 
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.TopicBuilder
+
+@Configuration
 class AutoCreateConfig {
+
+    @Bean
+    fun createNewTopic(): NewTopic {
+       return TopicBuilder
+                .name("library-events")
+                .partitions(3)
+                .replicas(3)
+                .build()
+    }
 }
