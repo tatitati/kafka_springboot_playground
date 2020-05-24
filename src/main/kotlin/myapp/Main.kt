07b38425145209a8d6fd9387da3fa48e29236cf8
tatitati <francis.albusac@simplybusiness.co.uk>
2020-05-25 00:44:45 +0100

    Mon May 25 00:44:45 BST 2020

--- src/main/kotlin/myapp/Main.kt
+++ src/main/kotlin/myapp/Main.kt
@@ -2,10 +2,8 @@ package myapp
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-@ComponentScan("infrastructure")
 class Main
 
 fun main(args: Array<String>) {
