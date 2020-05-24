168faf7683d887e27e9fb238ceaf3e9283526426
tatitati <francis.albusac@simplybusiness.co.uk>
2020-05-24 01:40:32 +0100

    Sun May 24 01:40:32 BST 2020

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,39 +1,65 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.3.0.RELEASE"
-	id("io.spring.dependency-management") version "1.0.9.RELEASE"
-	kotlin("jvm") version "1.3.72"
-	kotlin("plugin.spring") version "1.3.72"
+	id("org.springframework.boot") version "2.3.0.RELEASE" apply false
+	id("io.spring.dependency-management") version "1.0.9.RELEASE" apply false
+	id("org.jetbrains.kotlin.jvm") version "1.3.72" // == kotlin("jvm") version "1.3.72" apply false
+	id("org.jetbrains.kotlin.plugin.spring") version "1.3.72" // == kotlin("plugin.spring") version "1.3.72" apply false
 }
 
-group = "com.example"
-version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_1_8
+subprojects{
+	apply(plugin = "org.springframework.boot")
+	apply(plugin = "io.spring.dependency-management")
+	apply(plugin = "org.jetbrains.kotlin.jvm")
+	apply(plugin = "org.jetbrains.kotlin.plugin.spring")
 
-repositories {
-	mavenCentral()
-}
 
-dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-	implementation("org.jetbrains.kotlin:kotlin-reflect")
-	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-	implementation("org.springframework.kafka:spring-kafka")
-	testImplementation("org.springframework.boot:spring-boot-starter-test") {
-		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	group = "com.example"
+	version = "0.0.1-SNAPSHOT"
+
+	java {
+		sourceCompatibility = JavaVersion.VERSION_1_8
+		targetCompatibility = JavaVersion.VERSION_1_8
 	}
-	testImplementation("org.springframework.kafka:spring-kafka-test")
+
+	repositories {
+		mavenCentral()
+	}
+
+	dependencies {
+		implementation("org.springframework.boot:spring-boot-starter-web")
+		implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
+		implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+		implementation("org.springframework.kafka:spring-kafka")
+		testImplementation("org.springframework.boot:spring-boot-starter-test") {
+			exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+		}
+		testImplementation("org.springframework.kafka:spring-kafka-test")
+	}
+
+	tasks.withType<Test> {
+		useJUnitPlatform()
+	}
+
+	tasks.withType<KotlinCompile> {
+		kotlinOptions {
+			freeCompilerArgs = listOf("-Xjsr305=strict")
+			jvmTarget = "1.8"
+		}
+	}
 }
 
-tasks.withType<Test> {
-	useJUnitPlatform()
+
+
+project(":subprojects:ui"){
+	dependencies{
+		implementation(":subprojects:infrastructure")
+	}
 }
 
-tasks.withType<KotlinCompile> {
-	kotlinOptions {
-		freeCompilerArgs = listOf("-Xjsr305=strict")
-		jvmTarget = "1.8"
+project(":subprojects:infrastructure"){
+	dependencies{
+		implementation(":subprojects:domain")
 	}
 }
